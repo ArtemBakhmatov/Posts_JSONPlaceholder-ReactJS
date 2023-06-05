378bed7a01d11b7838b6a260500e9bba86f46f1d
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-06-05 20:51:18 +0300

    Фильтрация по поиску

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import PostList from './components/postList/PostList';
 import PostForm from './components/postForm/PostForm';
 import MySelect from './components/select/MySelect';
+import MyInput from './components/Input/MyInput';
 
 const App = () => {
 	const [posts, setPosts] = useState([
@@ -10,6 +11,7 @@ const App = () => {
 		{id: 3, title: 'CSS', body: 'tty54yg'}
 	]);
 	const [selectedSort, setSelectedSort] = useState('');
+	const [searchQuery, setSearchQuery] = useState('');
 
 	const createPost = (newPost) => {   // Добавить пост
 		setPosts([...posts, newPost]);
@@ -19,15 +21,30 @@ const App = () => {
 		setPosts(posts.filter(p => p.id !== post.id));
 	}
 
+	const sortedPosts = useMemo(() => {
+		if (selectedSort) {
+			return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+		}
+		return posts;
+	}, [selectedSort, posts]);
+
+	const sortedAndSearchedPosts = useMemo(() => {
+		return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery));
+	}, [searchQuery, sortedPosts]);
+
 	const sortPosts = (sort) => {
 		setSelectedSort(sort);
-		setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
 	}
 
 	return (
 		<div className='app'>
 			<PostForm create={createPost} />
 			<hr style={{margin: '15px 0'}} />
+			<MyInput 
+				value={searchQuery}
+				onChange={e => setSearchQuery(e.target.value)}
+				placeholder='Поиск...'
+			/>
 			<MySelect
 				value={selectedSort}
 				onChange={sortPosts}
@@ -37,9 +54,9 @@ const App = () => {
 					{value: 'body', name: 'По описанию'}
 				]}
 			/>
-			{posts.length !== 0
+			{sortedAndSearchedPosts.length !== 0
 				?
-					<PostList remove={removePost} posts={posts} title="Programming posts"/>
+					<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Programming posts"/>
 				:
 				<div style={{textAlign: 'center', fontWeight: 'bold', fontSize: '18px'}}>
 					Посты не найдены
