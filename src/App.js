7694fa7f1a2a953f41ac201f797d78087e6adaa0
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-06-05 20:12:27 +0300

    Сортировка поста

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { useState } from 'react';
 import PostList from './components/postList/PostList';
 import PostForm from './components/postForm/PostForm';
+import MySelect from './components/select/MySelect';
 
 const App = () => {
 	const [posts, setPosts] = useState([
-		{id: 1, title: 'JavaScript', body: 'Description'},
-		{id: 2, title: 'HTML', body: 'Description'},
-		{id: 3, title: 'CSS', body: 'Description'}
+		{id: 1, title: 'JavaScript', body: 'dwddqa'},
+		{id: 2, title: 'HTML', body: 'fdgerhgehon'},
+		{id: 3, title: 'CSS', body: 'tty54yg'}
 	]);
+	const [selectedSort, setSelectedSort] = useState('');
 
-	const createPost = (newPost) => {
+	const createPost = (newPost) => {   // Добавить пост
 		setPosts([...posts, newPost]);
 	}
 
-	const removePost = (post) => {
+	const removePost = (post) => {		// Удалить пость
 		setPosts(posts.filter(p => p.id !== post.id));
+	}
+
+	const sortPosts = (sort) => {
+		setSelectedSort(sort);
+		setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
 	}
 
 	return (
 		<div className='app'>
 			<PostForm create={createPost} />
+			<hr style={{margin: '15px 0'}} />
+			<MySelect
+				value={selectedSort}
+				onChange={sortPosts}
+				defaultValue='Сортирвка'
+				options={[
+					{value: 'title', name: 'По названию'},
+					{value: 'body', name: 'По описанию'}
+				]}
+			/>
 			{posts.length !== 0
 				?
 					<PostList remove={removePost} posts={posts} title="Programming posts"/>
---
+++ src/components/select/MySelect.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MySelect = ({value, onChange, defaultValue, options}) => {
+    return (
+        <select 
+            value={value}
+            onChange={e => onChange(e.target.value)}
+        >
+            <option disabled value=''>{defaultValue}</option>
+            {options.map(option => 
+                <option key={option.name} value={option.value}>
+                    {option.name}
+                </option>    
+            )}
+        </select>
+    );
+};
+
+export default MySelect;
