7a393662a48882864711c4eeb5e41beb82d9b3e9
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-06-05 13:16:20 +0300

    Передача пропса от родителя к ребенку от App.js к PostForm.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PostList from './components/postList/PostList';
-import MyButton from './components/button/MyButton';
-import MyInput from './components/Input/MyInput';
+import PostForm from './components/postForm/PostForm';
 
 const App = () => {
 	const [posts, setPosts] = useState([
@@ -9,36 +8,14 @@ const App = () => {
 		{id: 2, title: 'HTML', body: 'Description'},
 		{id: 3, title: 'CSS', body: 'Description'}
 	]);
-	const [post, setPost] = useState({title: '', body: ''});
 
-	const addNewPost = (e) => {
-		e.preventDefault();
-		setPosts([...posts, {...post, id: Date.now()}]);
-		setPost({title: '', body: ''});
-		
-	}
-
-	const onChangleInputTitle = (e) => {
-		setPost({...post, title: e.target.value});
-	}
-
-	const onChangleInputBody = (e) => {
-		setPost({...post, body: e.target.value});
+	const createPost = (newPost) => {
+		setPosts([...posts, newPost]);
 	}
 
 	return (
 		<div className='app'>
-			<form>
-				<MyInput 
-					value={post.title} 
-					onChange={e => onChangleInputTitle(e)}
-					placeholder='Название поста' />
-				<MyInput 
-					value={post.body}
-					onChange={e => onChangleInputBody(e)}
-					placeholder='Описание поста'/>
-				<MyButton onClick={e => addNewPost(e)} />
-			</form>
+			<PostForm create={createPost} />
 			<PostList posts={posts} title="Programming posts"/>
 		</div>
 	);
---
+++ src/components/postForm/PostForm.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState } from 'react';
+import MyButton from '../button/MyButton';
+import MyInput from '../Input/MyInput';
+
+const PostForm = ({create}) => {
+    const [post, setPost] = useState({title: '', body: ''});
+
+	const addNewPost = (e) => {
+		e.preventDefault();
+        const newPost = {...post, id: Date.now()};
+		create(newPost);
+        setPost({title: '', body: ''});
+	}
+
+	const onChangleInputTitle = (e) => {
+		setPost({...post, title: e.target.value});
+	}
+
+	const onChangleInputBody = (e) => {
+		setPost({...post, body: e.target.value});
+	}
+    return (
+        <form>
+            <MyInput 
+                value={post.title} 
+                onChange={e => onChangleInputTitle(e)}
+                placeholder='Название поста' />
+            <MyInput 
+                value={post.body}
+                onChange={e => onChangleInputBody(e)}
+                placeholder='Описание поста'/>
+            <MyButton onClick={e => addNewPost(e)} />
+        </form>
+    );
+};
+
+export default PostForm;
